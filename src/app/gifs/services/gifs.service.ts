34d1a8e6904f6b00cd9928ab1ffc0bb12d23afd5
Alex Hector Saavedra Manriquez <alexsaavedra.contacto@gmail.com>
2023-10-26 14:12:14 -0400

    gifs secction end

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+  public gifList: Gif[] = [];
+
+  private _tagsHistory: string[] = [];
+
+  private apiKey: string = '0Om5Xtl8jAyMSWFuBGPXC0L0GjWjr9K7';
+
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs service Ready');
+  }
+
+  get tagHistory() {
+    return [...this._tagsHistory];
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this.tagHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagsHistory.length === 0) return;
+
+    this.searchTag(this._tagsHistory[0]);
+  }
+
+  private organiceHistory(tag: string) {
+    tag = tag.toLocaleLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldtag) => oldtag !== tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+
+    this.saveLocalStorage();
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length == 0) {
+      return;
+    }
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.organiceHistory(tag);
+
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search?`, { params: params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
+        console.log({ gifs: this.gifList });
+      });
+  }
+}
---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+})
+export class SidebarComponent {
+  constructor(private gifService: GifsService) {}
+
+  get tagHistory(): string[] {
+    return this.gifService.tagHistory;
+  }
+
+  searchTag(tag: string) {
+    this.gifService.searchTag(tag);
+  }
+}
